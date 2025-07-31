c6687cb5588504a6ff458ccd1bca4ad5c5c3c216
Iosif Mazan <iosif.mazan@assisterr.ai>
2025-07-31 18:20:44 +0300

    refactor: Change DeliveryBaseSchema fields to allow None values for radius, delivery time, and price

--- schemas/admin/delivery.py
+++ schemas/admin/delivery.py
@@ -4,16 +4,18 @@ from pydantic import BaseModel, Field
 
 
 class DeliveryBaseSchema(BaseModel):
-    radius_from: float = Field(
-        default=..., description="Radius from in kilometers", gt=0
+    radius_from: float | None = Field(
+        default=None, description="Radius from in kilometers", gt=0
     )
-    radius_to: float = Field(default=..., description="Radius to in kilometers", gt=0)
-
-    delivery_time: int = Field(
-        default=..., description="Delivery time in minutes", gt=0
+    radius_to: float | None = Field(
+        default=None, description="Radius to in kilometers", gt=0
     )
 
-    price: Decimal = Field(default=..., description="Price", gt=0)
+    delivery_time: int | None = Field(
+        default=None, description="Delivery time in minutes", gt=0
+    )
+
+    price: Decimal | None = Field(default=None, description="Price", gt=0)
 
 
 class DeliveryCreateSchema(DeliveryBaseSchema):
