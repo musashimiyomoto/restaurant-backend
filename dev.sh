7aef337daa4b2dddedd50e5c3814b39774c006f4
Iosif Mazan <josephmazan6174@gmail.com>
2025-08-02 12:10:26 -0700

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -193,9 +193,8 @@ build_docker() {
         print_warning "Container status:"
         docker-compose ps
         echo ""
-        print_success "API available at: http://localhost:8000"
+        print_success "API available at: http://localhost:5000"
         print_success "DB UI available at: http://localhost:8080"
-        print_success "Broker UI available at: http://localhost:3000"
         print_success "Redis UI available at: http://localhost:5540"
     else
         print_error "Error starting containers"
